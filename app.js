ca9912761a7e35afdf88baf4ff86ade494eab5ec
Russ Reed <61715274+rureed@users.noreply.github.com>
2020-08-13 15:56:10 -0400

    fix functions, add styling

--- app.js
+++ app.js
@@ -10,111 +10,127 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
+
+buildTeam();
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const manager = [
-    {
-        type: "input",
-        name: "name",
-        message: "Manager's name:"
-    },
-    {
-        type: "input",
-        name: "ID",
-        message: "Manager's ID:"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Manager's Email address:"
-    },
-    {
-        type: "input",
-        name: "office",
-        message: "Manager's Office number:"
-    },
-];
-
-const engineer = [
-    {
-        type: "input",
-        name: "name",
-        message: "Engineer's name:"
-    },
-    {
-        type: "input",
-        name: "ID",
-        message: "Engineer's ID:"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Engineer's Email address:"
-    },
-    {
-        type: "input",
-        name: "office",
-        message: "Engineer's GitHub username:"
-    },
-];
-
-const intern = [
-    {
-        type: "input",
-        name: "name",
-        message: "Intern's name:"
-    },
-    {
-        type: "input",
-        name: "ID",
-        message: "Intern's ID:"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Intern's Email address:"
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "Intern's school:"
-    },
-];
-
-const chooseNext = [
-    {
-        type: "list",
-        name: "newEmployee",
-        message: "Select next employee:",
-        choices: [
-            "Engineer",
-            "Intern",
-            "None"
-        ]
-    },
-];
-
-function init() {
-    inquirer
-        .prompt(questions)
-        .then(answers => {
-            console.log(answers);
-            axios.get("https://api.github.com/users/"+answers.name)
-                .then(response => {
-                    var imageUrl = response.data.avatar_url;
-                    answers.image = imageUrl;
-                    fs.writeFile("ReadMe.md", generateMarkdown(answers), function (err) {
-                        if (err) {
-                            throw err;
-                        };
-                        console.log("New ReadMe has been generated.")
-
-                    })
-                })
-        })
+async function newManager() {
+    let answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Manager's name:"
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Manager's ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Manager's Email address:"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "Manager's Office number:"
+        }
+    ])
+    return new Manager(answers.name, answers.id, answers.email, answers.office);
 }
+
+async function newEngineer() {
+    let answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Engineer's name:"
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Engineer's ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Engineer's Email address:"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "Engineer's GitHub username:"
+        }
+    ]);
+    return new Engineer(answers.name, answers.id, answers.email, answers.github);
+}
+
+async function newIntern() {
+    let answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Intern's name:"
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Intern's ID:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Intern's Email address:"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Intern's school:"
+        }
+    ]);
+    return new Intern(answers.name, answers.id, answers.email, answers.school);
+}
+
+async function createNewEmployee() {
+    let answers = await inquirer.prompt([
+        {
+            type: "list",
+            name: "new",
+            message: "Select next employee:",
+            choices: [
+                "Engineer",
+                "Intern",
+                "None"
+            ]
+        }
+    ])
+    switch (answers.new) {
+        case "Engineer": return newEngineer()
+        case "Intern": return newIntern()
+        default: return null
+    }
+}
+
+async function buildTeam() {
+    console.log("Add team members:")
+    team.push(await newManager())
+    do {
+        var newEmployee = await createNewEmployee();
+        if (newEmployee) {
+            team.push(newEmployee);
+        }
+    } while (newEmployee)
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(team));
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
